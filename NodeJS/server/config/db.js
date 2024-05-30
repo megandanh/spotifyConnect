422c8ba5013a7498a765d9bfcdca3c9627bd8a20
maria <mariafmauco@gmail.com>
2024-05-29 22:37:23 -0400

    Update db.js

--- NodeJS/server/config/db.js
+++ NodeJS/server/config/db.js
@@ -12,17 +12,3 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB; 
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  
-  try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-
-module.exports = connectDB; 
